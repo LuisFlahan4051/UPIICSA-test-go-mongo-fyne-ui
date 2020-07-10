521b319222a56da21cc3a4badbfc770e827e6339
LuisFlahan4051 <luisflahan4051@gmail.com>
2020-07-10 05:50:02 -0500

    Agregue el estilo a la ventana splash e inicie con la estructura del formulario

--- main.go
+++ main.go
@@ -1,33 +1,71 @@
 package main
 
 import (
+	"log"
 	"time"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/driver/desktop"
 	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
+	"golang.org/x/image/colornames"
 )
 
 func main() {
-	//Win Init and congfig
+	//Win Init
 	myApp := app.New()
+
 	myWindow := myApp.NewWindow("Proyecto LuisFlahan4051 Registro de usuarios")
 
 	myDriverApp := fyne.CurrentApp().Driver()
 	driverDesktop, _ := myDriverApp.(desktop.Driver)
 	windowSplash := driverDesktop.CreateSplashWindow()
 
-	//widgets
-	btn1 := widget.NewButton("btn1", func() {
-		w := fyne.CurrentApp().Driver().CreateWindow("Hello2")
-		w.Show()
-	})
+	//Elementos
+	inputNombre := widget.NewEntry()
+	inputNombre.SetPlaceHolder("Nombre completo...")
+	formItemNombre := widget.NewFormItem("Nombre:", inputNombre)
 
-	//Layout
-	row1 := layout.NewVBoxLayout()
-	contenedor := fyne.NewContainerWithLayout(row1, btn1)
+	inputApellidos := widget.NewEntry()
+	inputApellidos.SetPlaceHolder("Ingrese sus dos apellidos...")
+	formItemApellidos := widget.NewFormItem("Apellidos:", inputApellidos)
+
+	form := widget.NewForm(
+		formItemNombre,
+		formItemApellidos,
+	)
+	form.OnSubmit = func() {
+		log.Println("Form submited:", inputNombre.Text)
+	}
+
+	//--
+	txtBienvenida := canvas.NewText("~* ¡Bienvenido! *~", colornames.Cyan)
+	txtBienvenida.Alignment = fyne.TextAlignCenter
+	txtBienvenida.TextStyle = fyne.TextStyle{Bold: true}
+	txtBienvenida.TextSize = 38
+
+	image := canvas.NewImageFromFile("src/luisflahan4051apps.png")
+	image.FillMode = canvas.ImageFillOriginal
+
+	//Layouts init
+	centrado := layout.NewCenterLayout()
+	vertical := layout.NewVBoxLayout()
+
+	//Disposiciones
+	contenedor := fyne.NewContainerWithLayout(vertical, form)
+
+	//--
+
+	cajaBienvenida := widget.NewVBox(
+		txtBienvenida,
+		image,
+	)
+	contenedorBienvenida := fyne.NewContainerWithLayout(centrado,
+		cajaBienvenida,
+	)
 
 	//Win Config
 
@@ -35,13 +73,10 @@ func main() {
 	myWindow.SetFixedSize(true)
 	myWindow.SetContent(contenedor)
 
-	windowSplash.Resize(fyne.NewSize(400, 200))
-	windowSplash.SetContent(
-		widget.NewLabelWithStyle(
-			"Bienvenido!\n\n*~ Luisflahan4051 APPS ~*",
-			fyne.TextAlignCenter, fyne.TextStyle{Bold: true},
-		),
-	)
+	//----
+	windowSplash.Resize(fyne.NewSize(500, 300))
+	windowSplash.CenterOnScreen()
+	windowSplash.SetContent(contenedorBienvenida)
 
 	//Show and run
 	windowSplash.Show()
@@ -49,9 +84,11 @@ func main() {
 	go func() {
 		time.Sleep(time.Second * 3)
 		windowSplash.Hide()
+		myApp.Settings().SetTheme(theme.DarkTheme())
 		myWindow.Show()
 		windowSplash.Close()
 	}()
 
+	myApp.Settings().SetTheme(theme.LightTheme())
 	myApp.Run()
 }
